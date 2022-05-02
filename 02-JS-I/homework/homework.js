63c4030d5d4a7eb69ff4b32f3012dbe7b935b006
bebush <bebu100@hotmal.com>
2022-05-02 18:19:49 -0300

    sabado

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = 'emmmaaaaa';
+const nuevaString = 'hoy me combierto en héroe';
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = 6;
